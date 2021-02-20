8ca79c5f65c6bba4016fc5fadb1337e993b01d08
primenumber <prime@kmc.gr.jp>
2021-02-20 16:48:59 +0900

    Section 6 - Surface Normals and Multiple Objects

--- main.cpp
+++ main.cpp
@@ -1,30 +1,18 @@
 #include <iostream>
 
+#include "rtweekend.hpp"
+
 #include "color.hpp"
-#include "ray.hpp"
-#include "vec3.hpp"
+#include "hittable_list.hpp"
+#include "sphere.hpp"
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-  vec3 oc = r.origin() - center;
-  auto a = r.direction().norm();
-  auto half_b = dot(oc, r.direction());
-  auto c = oc.norm() - radius * radius;
-  auto discriminant = half_b*half_b - a*c;
-  if (discriminant < 0) {
-    return -1.0;
-  } else {
-    return (-half_b - sqrt(discriminant)) / a;
-  }
-}
-
-color ray_color(const ray& r) {
-  auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-  if (t > 0.0) {
-    vec3 N = normalize(r.at(t) - vec3(0,0,-1));
-    return 0.5 * color(N.x()+1, N.y()+1, N.z()+1);
+color ray_color(const ray& r, const hittable& world) {
+  hit_record rec;
+  if (world.hit(r, 0, infinity, rec)) {
+    return 0.5 * (rec.normal + color(1,1,1));
   }
   vec3 unit_direction = normalize(r.direction());
-  t = 0.5 * (unit_direction.y() + 1.0);
+  auto t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0-t) * color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
@@ -34,6 +22,11 @@ int main() {
   const auto aspect_ratio = 16.0 / 9.0;
   const ptrdiff_t image_width = 400;
   const ptrdiff_t image_height = static_cast<ptrdiff_t>(image_width / aspect_ratio);
+
+  // World
+  hittable_list world;
+  world.add(std::make_shared<sphere>(point3(0,0,-1), 0.5));
+  world.add(std::make_shared<sphere>(point3(0,-100.5,-1),100));
 
   // Camera
   
@@ -55,7 +48,7 @@ int main() {
       auto u = double(i) / (image_width-1);
       auto v = double(j) / (image_height-1);
       ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-      color pixel_color = ray_color(r);
+      color pixel_color = ray_color(r, world);
       write_color(std::cout, pixel_color);
     }
   }
