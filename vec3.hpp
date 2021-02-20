b0212d9bb7ea45829ce3968941636dc1385bb875
primenumber <prime@kmc.gr.jp>
2021-02-20 19:50:07 +0900

    Section 12 - Deforcus Blur

--- vec3.hpp
+++ vec3.hpp
@@ -143,4 +143,12 @@ inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
   return r_out_perp + r_out_parallel;
 }
 
+inline vec3 random_in_unit_disk() {
+  while (true) {
+    auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+    if (p.norm() >= 1) continue;
+    return p;
+  }
+}
+
 #endif // VEC3_HPP
