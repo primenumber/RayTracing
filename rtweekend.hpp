1fac643372fc22f3a04da8a8fb61b2c5d98b1540
primenumber <prime@kmc.gr.jp>
2021-02-20 17:15:14 +0900

    Implement random_double

--- rtweekend.hpp
+++ rtweekend.hpp
@@ -3,12 +3,24 @@
 
 #include <cmath>
 #include <limits>
+#include <random>
 
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+  static std::uniform_real_distribution<double> dis(0.0, 1.0);
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  return dis(gen);
+}
+
+inline double random_double(double min, double max) {
+  return min + (max-min) * random_double();
 }
 
 #include "ray.hpp"
