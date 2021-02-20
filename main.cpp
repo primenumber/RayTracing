8688821720861f5a9590e96bbced902d812fb139
primenumber <prime@kmc.gr.jp>
2021-02-20 17:16:00 +0900

    Section 7 - Antiailasing

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "color.hpp"
 #include "hittable_list.hpp"
 #include "sphere.hpp"
+#include "camera.hpp"
 
 color ray_color(const ray& r, const hittable& world) {
   hit_record rec;
@@ -22,6 +23,7 @@ int main() {
   const auto aspect_ratio = 16.0 / 9.0;
   const ptrdiff_t image_width = 400;
   const ptrdiff_t image_height = static_cast<ptrdiff_t>(image_width / aspect_ratio);
+  const size_t samples_per_pixel = 100;
 
   // World
   hittable_list world;
@@ -30,13 +32,7 @@ int main() {
 
   // Camera
   
-  auto viewport_height = 2.0;
-  auto viewport_width = aspect_ratio * viewport_height;
-  auto focal_length = 1.0;
-  auto origin = point3(0, 0, 0);
-  auto horizontal = vec3(viewport_width, 0, 0);
-  auto vertical = vec3(0, viewport_height, 0);
-  auto lower_left_corner = origin - horizontal / 2. - vertical / 2. - vec3(0, 0, focal_length);
+  camera cam;
 
   // Render
 
@@ -45,11 +41,14 @@ int main() {
   for (ptrdiff_t j = image_height-1; j >= 0; --j) {
     std::cerr << "\rScanlines ramaining: " << j << ' ' << std::flush;
     for (ptrdiff_t i = 0; i < image_width; ++i) {
-      auto u = double(i) / (image_width-1);
-      auto v = double(j) / (image_height-1);
-      ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-      color pixel_color = ray_color(r, world);
-      write_color(std::cout, pixel_color);
+      color pixel_color(0, 0, 0);
+      for (size_t s = 0; s < samples_per_pixel; ++s) {
+        auto u = (i + random_double()) / (image_width-1);
+        auto v = (j + random_double()) / (image_height-1);
+        ray r = cam.get_ray(u, v);
+        pixel_color += ray_color(r, world);
+      }
+      write_color(std::cout, pixel_color, samples_per_pixel);
     }
   }
 
