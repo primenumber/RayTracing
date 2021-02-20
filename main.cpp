07a2c0467debf247affa052cacc99c74b6862dbb
primenumber <prime@kmc.gr.jp>
2021-02-20 19:42:38 +0900

    Section 11 - Positionable Camera

--- main.cpp
+++ main.cpp
@@ -37,22 +37,23 @@ int main() {
   const int32_t max_depth = 50;
 
   // World
+
   hittable_list world;
 
   auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
   auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
   auto material_left   = std::make_shared<dielectric>(1.5);
-  auto material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+  auto material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
   world.add(std::make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
   world.add(std::make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
   world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-  world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
+  world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
   world.add(std::make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
   // Camera
   
-  camera cam;
+  camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20.0, aspect_ratio);
 
   // Render
 
