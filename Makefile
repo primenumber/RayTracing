0e3dc6e0da24531cb8dfcb36b984b5379fcc02bd
primenumber <prime@kmc.gr.jp>
2021-02-20 16:48:09 +0900

    Implement hittable_list

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CXXFLAGS:=-std=c++17 -Og -Wall -Wextra
 TARGET:=rtiow
-SRCS:=main.cpp sphere.cpp
+SRCS:=main.cpp sphere.cpp hittable_list.cpp
 OBJS:=$(SRCS:%.cpp=%.o)
 
 $(TARGET): $(OBJS)
@@ -9,5 +9,6 @@ $(TARGET): $(OBJS)
 %.o: %.cpp
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
 
-main.o: vec3.hpp color.hpp ray.hpp hittable.hpp sphere.hpp
+main.o: vec3.hpp color.hpp ray.hpp hittable.hpp sphere.hpp hittable_list.hpp
 sphere.o: vec3.hpp ray.hpp hittable.hpp sphere.hpp
+hittable_list.o: vec3.hpp ray.hpp hittable.hpp hittable_list.hpp
---
+++ hittable_list.hpp
@@ -0,0 +1,23 @@
+#ifndef HITTABLE_LIST_HPP
+#define HITTABLE_LIST_HPP
+
+#include "hittable.hpp"
+
+#include <memory>
+#include <vector>
+
+class hittable_list : public hittable {
+ public:
+  hittable_list() {}
+  explicit hittable_list(std::shared_ptr<hittable> object) { add(object); }
+
+  void clear() { objects.clear(); }
+  void add(std::shared_ptr<hittable> object) { objects.push_back(object); }
+
+  virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+ public:
+  std::vector<std::shared_ptr<hittable>> objects;
+};
+
+#endif // HITTABLE_LIST_HPP
