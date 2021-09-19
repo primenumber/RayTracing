b5206b23a799ed532cdaa3bd2adb99e29de74bf9
primenumber <prime@kmc.gr.jp>
2021-09-20 06:41:04 +0900

    Use padding for vectorize

--- vec3.hpp
+++ vec3.hpp
@@ -8,8 +8,9 @@
 
 class vec3 {
  public:
-  vec3() : e{0, 0, 0} {}
-  vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+  vec3() : e{0, 0, 0, 0} {}
+  vec3(double e0, double e1, double e2) : e{e0, e1, e2, 0} {}
+  vec3(double e0, double e1, double e2, double e3) : e{e0, e1, e2, e3} {}
 
   double x() const { return e[0]; }
   double y() const { return e[1]; }
@@ -51,7 +52,7 @@ class vec3 {
   }
 
  public:
-  double e[3];
+  double e[4];
 };
 
 using point3 = vec3;  // 3D point
