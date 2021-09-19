3df661ec444def6bd470c3bbc100d7dcec8641cc
primenumber <prime@kmc.gr.jp>
2021-09-19 10:53:21 +0900

    Optimize aabb

--- aabb.cpp
+++ aabb.cpp
@@ -3,10 +3,13 @@
 bool aabb::hit(const ray& r, double t_min, double t_max) const {
   for (size_t a = 0; a < 3; ++a) {
     auto invD = 1.0 / r.direction()[a];
-    auto t0 = fmin((minimum[a] - r.origin()[a]) * invD, (maximum[a] - r.origin()[a]) * invD);
-    auto t1 = fmax((minimum[a] - r.origin()[a]) * invD, (maximum[a] - r.origin()[a]) * invD);
-    t_min = fmax(t0, t_min);
-    t_max = fmin(t1, t_max);
+    auto t0 = (min()[a] - r.origin()[a]) * invD;
+    auto t1 = (max()[a] - r.origin()[a]) * invD;
+    if (invD < 0.0) {
+      std::swap(t0, t1);
+    }
+    t_min = std::max(t0, t_min);
+    t_max = std::min(t1, t_max);
     if (t_max <= t_min) return false;
   }
   return true;
